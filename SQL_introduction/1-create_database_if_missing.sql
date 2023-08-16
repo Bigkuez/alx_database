355686b76d6d46d62f760f674664d5460c08534d
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-16 16:47:06 +0100

    commit

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
--- Delete and create database hbtn_0c_0
+-- This is to Delete existing database
 DROP DATABASE IF EXISTS hbtn_0c_0;
+--This is create database hbtn_0c_0
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
