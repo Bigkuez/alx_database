1305a8158c566795a4388483fd99cdd94ab98c00
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-18 13:53:24 +0100

    commit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_4;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
-USE hbtn_test_db_4;
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
 
 
 -- This is to create a new table
