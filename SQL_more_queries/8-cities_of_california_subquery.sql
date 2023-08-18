3357cfb3af2de5ad4321b3123a06c95a293b99b9
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-18 12:16:45 +0100

    commit

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,7 @@
--- This is for database creation with table 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `cities`
-(
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-	state_id INT NOT NULL,
-	name VARCHAR(256),
-	FOREIGN KEY(state_id) REFERENCES states(id)
-);
+-- This is selecting using subquery
+SELECT id, name FROM cities
+WHERE state_id = (
+SELECT id FROM states
+WHERE name = 'California'
+)
+ORDER BY cities.id
