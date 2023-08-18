74e5bc7fc61f391560f74c45151d378f0f57a5cb
Amakuez <99471306+Amakuez@users.noreply.github.com>
2023-08-18 13:26:02 +0100

    commit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,8 @@
 -- This script creates a table called first_table in the specified database.
-DROP DATABASE IF EXISTS hbtn_test_db;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Replace 'hbtn_test' with the actual database name when running the script
-USE hbtn_test_db;
+USE hbtn_0c_0;
 
 -- Create the table first_table if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (
